43ae3b382dcb7e2251f4bbd2f724425df55709d0
Roman Puchinsky <romanpu@gmail.com>
2024-11-24 12:55:45 +0200

    coonected socket with server

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ import { loggerService } from './services/logger.service.js'
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import path from 'path'
 
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+});
 
 const corsOptions = {
     origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
@@ -30,15 +41,24 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/notification', notificationRoutes)
 
-// fallback route
-// app.get('/**', (req, res) => {
-//     console.log('fallback route')
-//     res.sendFile(path.resolve('public/index.html'))
-// })
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('notification', (notification) => {
+        console.log('notification', notification)
+    })
+})
+
+//fallback route
+app.get('/**', (req, res) => {
+    console.log('fallback route')
+    res.sendFile(path.resolve('public/index.html'))
+})
 
 const PORT = process.env.PORT || 3030
 console.log(process.env.PORT)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     loggerService.info('Up and running on port ' + PORT)
 })
+
+   
